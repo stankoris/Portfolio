b293a7a27b439a90f8eedee33761cca2c7f5a523
stankoris <risimicstanko@gmail.com>
2024-06-23 14:33:41 +0200

    prepravljen bag u indexu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const listButton = document.getElementById("listButton");
 const listMobile = document.getElementById("listMobile");
 const closeButton = document.getElementById("closeButton");
 const mainTag = document.querySelector('main');
+const background = document.getElementById("indexBackground");
 
 let list_open = false;
 
@@ -12,6 +13,7 @@ listButton.addEventListener("click", () => {
         listMobile.style.transform = "translateX(0%)";
         listButton.style.display = "none";
         mainTag.style.zIndex = "0";
+        background.style.display = "none";
     } else {
         listMobile.style.transform = "translateX(-100%)";
     }
@@ -24,6 +26,7 @@ closeButton.addEventListener("click", () => {
         listMobile.style.transform = "translateX(-100%)";
         listButton.style.display = "block"
         listButton.style.transition = "display 1s ease";
+        background.style.display = "block";
         mainTag.style.zIndex = "2000";
     }
 });
