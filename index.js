aa2792ced76e290c0e8d24a43828180723e75c7b
stankoris <risimicstanko@gmail.com>
2024-06-24 11:47:46 +0200

    dodata light tema

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const listButton = document.getElementById("listButton");
 const listMobile = document.getElementById("listMobile");
 const closeButton = document.getElementById("closeButton");
 const mainTag = document.querySelector('main');
-const background = document.getElementById("indexBackground");
+const banner = document.getElementById("banner");
 
 let list_open = false;
 
@@ -13,12 +13,11 @@ listButton.addEventListener("click", () => {
         listMobile.style.transform = "translateX(0%)";
         listButton.style.display = "none";
         mainTag.style.zIndex = "0";
-        background.style.display = "none";
+        banner.style.display = "none";
     } else {
         listMobile.style.transform = "translateX(-100%)";
     }
 });
-
 
 closeButton.addEventListener("click", () => {
     if (list_open) {
@@ -26,7 +25,28 @@ closeButton.addEventListener("click", () => {
         listMobile.style.transform = "translateX(-100%)";
         listButton.style.display = "block"
         listButton.style.transition = "display 1s ease";
-        background.style.display = "block";
+        banner.style.display = "flex";
         mainTag.style.zIndex = "2000";
+    }
+});
+
+// KOD ZA PROMENU TEME
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+const toggleCircle = themeToggleBtn.querySelector('.toggle-circle');
+const blackBanner = document.getElementById("blackBanner");
+
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+    if (document.body.classList.contains('light-theme')) {
+        toggleCircle.classList.remove('moon-icon');
+        toggleCircle.classList.add('sun-icon');
+        banner.style.display = "none"
+        blackBanner.style.display = "flex"
+    } else {
+        toggleCircle.classList.remove('sun-icon');
+        toggleCircle.classList.add('moon-icon');
+        banner.style.display = "flex"
+                blackBanner.style.display = "none"
     }
 });
